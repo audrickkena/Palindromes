de412413d990c58c6d4737e6a7bc1173821e483e
Ken <83001955+audrickkena@users.noreply.github.com>
2021-06-23 02:35:47 +0800

    full implementation of palindrome check with punctuation and whitespace accounted for

--- main.cpp
+++ main.cpp
@@ -6,19 +6,24 @@
 using std::string; using std::cout;
 using std::cin; 
 
-// references isPalindrome function in palindromeCheck.cpp
-bool isPalindrome(const string& toCheck);
+// references function in palindromeCheck.cpp
+bool isPalindrome(string toCheck);
+// old implementation for word only
+//bool isWordPalindrome(const string& toCheck);
+
 
 int main() 
 {
 	int procLength;
 	string userInp;
-	cout << "Enter number of words to process: ";
+
+	// full implementation
+	cout << "Enter number of words/phrases/sentences to process: ";
 	cin >> procLength;
-	cin.ignore(); //cin leaves \n char in buffer so use cin.ignore to get rid of extra chars before using getline()
+	cin.ignore();
 	for (int i = 0; i < procLength; i++)
 	{
-		cout << "Enter palindrome to process: ";
+		cout << "Enter a palindrome to process: ";
 		getline(cin, userInp);
 		if (isPalindrome(userInp))
 		{
@@ -28,7 +33,25 @@ int main()
 		{
 			cout << userInp << " is not a palindrome\n";
 		}
-		
 	}
 	return 0;
+
+	// word implementation 
+	//cout << "Enter number of words to process: ";
+	//cin >> procLength;
+	//cin.ignore(); //cin leaves \n char in buffer so use cin.ignore to get rid of extra chars before using getline()
+	//for (int i = 0; i < procLength; i++)
+	//{
+	//	cout << "Enter a word palindrome to process: ";
+	//	getline(cin, userInp);
+	//	if (isWordPalindrome(userInp))
+	//	{
+	//		cout << userInp << " is a palindrome\n";
+	//	}
+	//	else
+	//	{
+	//		cout << userInp << " is not a palindrome\n";
+	//	}
+	//	
+	//}
 }
--- palindromeCheck.cpp
+++ palindromeCheck.cpp
@@ -1,20 +1,48 @@
 // checker file
 #include <iostream>
-using std::string;
+#include <algorithm> // for remove and remove_if
+#include <functional> // for std::not1
+#include <locale>
+#include <string>
 
-// using const reference param in function to avoid copying string and to make sure orig string not modified.
-bool isPalindrome(const string& toCheck)
+using std::string; using std::transform;
+using std::remove_copy_if; using std::back_inserter;
+using std::ptr_fun; using std::not1;
+
+// to use as predicate for what to remove in string input
+bool toRemove(unsigned char c) {
+	return (c == ' ' || c == '\n' || c == '\r' ||
+		c == '\t' || c == '\v' || c == '\f' || c == ',' ||
+		c == '.' || c == '/' || c == '!' || c == '?' || c == ':' ||
+		c == '-' || c == '--');
+}
+
+// to return a string that only contains lowercase alphabetical chars
+string cleanInp(const string &inp)
 {
-	if (toCheck.length() == 1)
+	string result;
+	result.reserve(inp.length());
+
+	// copies from inp everything except what returns pred as true, in this case, anything that is NOT an alphabeti char. it is inserted using back inserter iterator into result
+	remove_copy_if(inp.begin(), inp.end(), back_inserter(result), not1(ptr_fun(isalpha)));
+	transform(result.begin(), result.end(), result.begin(), ::tolower); // turn whole string to lowercase
+	return result;
+}
+
+// full implementation, copies param instead to allow for temp changes
+bool isPalindrome(string toCheck)
+{
+	if (toCheck.length() == 0)
 	{
 		return false;
 	}
 	else
 	{
-		int lim = (toCheck.length() / 2);
+		string cleanedInp = cleanInp(toCheck);
+		int lim = (cleanedInp.length() / 2);
 		for (int i = 0; i < lim; i++)
 		{
-			if (toCheck[i] != toCheck[toCheck.length() - i - 1])
+			if (cleanedInp[i] != cleanedInp[cleanedInp.length() - i - 1])
 			{
 				return false;
 			}
@@ -22,3 +50,24 @@ bool isPalindrome(const string& toCheck)
 		return true;
 	}
 }
+
+// using const reference param in function to avoid copying string and to make sure orig string not modified.
+//bool isWordPalindrome(const string& toCheck)
+//{
+//	if (toCheck.length() == 0)
+//	{
+//		return false;
+//	}
+//	else
+//	{
+//		int lim = (toCheck.length() / 2);
+//		for (int i = 0; i < lim; i++)
+//		{
+//			if (toCheck[i] != toCheck[toCheck.length() - i - 1])
+//			{
+//				return false;
+//			}
+//		}
+//		return true;
+//	}
+//}
